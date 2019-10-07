ab14f08749e2b964819fd022b5efa7fc741c19df
taruntula <tarunpadath@gmail.com>
2019-10-07 18:19:27 +0000

    use file_get_contents to grab the contents of the dummy data json file 'dummy-products_list.json', assign it to a variable called output in products.php

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,11 +1,15 @@
 <?php
+require_once('functions.php');
+set_exception_handler('error_handler');
 
-header('Content-Type: application/json');
+$output = file_get_contents('dummy-products-list.json');
+print($output);
+// header('Content-Type: application/json');
 
-if (empty($_GET['id'])) {
-  readfile('dummy-products-list.json');
-} else {
-  readfile('dummy-product-details.json');
-}
+// if (empty($_GET['id'])) {
+//   readfile('dummy-products-list.json');
+// } else {
+//   readfile('dummy-product-details.json');
+// }
 
 ?>
