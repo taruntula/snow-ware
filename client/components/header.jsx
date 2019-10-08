1ff754c2f065fc2be426677835f14ce1aefb072a
taruntula <tarunpadath@gmail.com>
2019-10-08 17:25:56 +0000

    Changed title in header

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Header(props) {
   return (
     <div className="row justify-content-center">
-      <h1 className="col-4">Tarun Supplies</h1>
+      <h1 className="col-4">Wicked Sales</h1>
       <h1 className="col-2 ">
         <i className="fas fa-store"></i>
       </h1>
