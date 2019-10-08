5de9839384cd51272c36f809a0809891f69a7480
taruntula <tarunpadath@gmail.com>
2019-10-08 01:02:16 +0000

    made images fit in card, still uneven heights for cards

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 function ProductListItem(props) {
   return (
     <div className="col-4">
-      <div className="thumbnail">
+      <div className="card">
         <h1 className="text-center">
-          <img src={props.image} alt="" />
+          <img className="card-img-top h-50" src={props.image} alt="" />
           {/* <i className="fas fa-cat"></i> */}
         </h1>
         {/* <img src="..server/public/images/example-product.png" alt=""/> */}
-        <div className="caption">
+        <div className="card-body h-50">
           <h3>{props.name}</h3>
           <h6>{props.price}</h6>
           <p>{props.description}</p>
