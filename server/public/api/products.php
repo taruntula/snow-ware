91adbb10f0c9e2de1af4a5698dbf8e822fd40878
taruntula <tarunpadath@gmail.com>
2019-10-08 00:24:59 +0000

    Added status code

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,18 +1,9 @@
 <?php
 require_once('functions.php');
-require_once('db_connection.php');
 set_exception_handler('error_handler');
+require_once('db_connection.php');
 
 $output = file_get_contents('dummy-products-list.json');
 print($output);
-
-
-// header('Content-Type: application/json');
-
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
 
 ?>
