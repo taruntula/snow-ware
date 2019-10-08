404740af5e62524d50a7dc66be407bcde16fca6b
taruntula <tarunpadath@gmail.com>
2019-10-08 23:30:12 +0000

    Connected to products table and fetch data

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -1,5 +1,7 @@
 <?php
-
+function startup() {
+  header('Content-Type:application/json');
+}
 function error_handler($error){
   $output = [
     'success' => false ,
@@ -9,5 +11,5 @@ function error_handler($error){
   $json_output = json_encode($output);
   print($json_output);
 }
-
+startup();
 ?>
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,8 +3,23 @@ require_once('functions.php');
 set_exception_handler('error_handler');
 require_once('db_connection.php');
 
+$query = "SELECT * FROM `products`";
+$result = mysqli_query($conn,$query);
 
-$output = file_get_contents('dummy-products-list.json');
-print($output);
+
+if(!$result){
+  throw new Exception('query error ' . mysqli_error($conn));
+}
+
+$output = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+  $row['id'] = intval($row['id']);
+  $row['price'] = intval($row['price']);
+  $output[] = $row;
+}
+
+$jsonData = json_encode($output);
+print($jsonData);
 
 ?>
