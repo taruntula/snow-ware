6b0e1065fc12649673ebcdd00c8fb00aef2634e7
taruntula <tarunpadath@gmail.com>
2019-10-11 18:51:58 +0000

    Completed checkout form and item total, need to be able to reset cart after submit button correctly

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -3,34 +3,38 @@ import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
   const cartArray = props.cart;
-  let sum = 0;
-  for (var integerI = 0; integerI < cartArray.length; integerI++) {
-    sum += parseInt(cartArray[integerI].price);
-  }
-  const formattedSum = '$' + (sum / 100).toFixed(2);
-  return (
-    <div className="container">
-      <div className="row mt-2">
-        <button onClick={() => props.view('catalog', {})} className="btn btn-link">
-          <h6> Back to catalog</h6>
-        </button>
-      </div>
-      <div className="row ml-1 mb-2">
-        <h2>My Cart</h2>
-      </div>
-      <div className="col-12 mb-2">
-        {cartArray.map(singleCartItem => {
-          return <CartSummaryItem key={singleCartItem.id} image={singleCartItem.image} name={singleCartItem.name} price={singleCartItem.price} description={singleCartItem.shortDescription} />;
-        })}
-      </div>
-      <div className="col-12 mt-6">
-        <div className="row">
-          <h2>Item Total {formattedSum}</h2>
+  const total = props.total;
+  if (cartArray.length === 0) {
+    return (<h1>Cart is Empty</h1>);
+  } else {
+    return (
+      <div className="container">
+        <div className="row mt-2">
+          <button onClick={() => props.view('catalog', {})} className="btn btn-link">
+            <h6> Back to catalog</h6>
+          </button>
+        </div>
+        <div className="row ml-1 mb-2">
+          <h2>My Cart</h2>
+        </div>
+        <div className="col-12 mb-2">
+          {cartArray.map(singleCartItem => {
+            return <CartSummaryItem key={singleCartItem.id} image={singleCartItem.image} name={singleCartItem.name} price={singleCartItem.price} description={singleCartItem.shortDescription} />;
+          })}
+        </div>
+        <div className="col-12 mt-6">
+          <div className="row">
+            <div className="col-9">
+              <h2>Item Total {total}</h2>
+            </div>
+            <div className="col-3">
+              <button className="btn btn-light" onClick={() => props.view('checkout', {})}>Checkout</button>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-
+    );
+  }
 }
 
 export default CartSummary;
