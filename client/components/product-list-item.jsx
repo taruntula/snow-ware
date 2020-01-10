82a90dc814100cc9c1f0462fcb049edbf3a33160
Tarun Padath <tarunpadath@gmail.com>
2020-01-10 01:22:03 -0800

    Changed font and made button stick to bottom of card

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -9,11 +9,11 @@ function ProductListItem(props) {
     <div className="col-4 mb-3 h-10 catalog-height">
       <div className="card h-100 see-through">
         <img className="card-img-top h-50 img-fluid p-3" src={`../../images/${props.image}`} alt="" />
-        <div className="card-body h-50 p-3">
+        <div className="card-body h-50 p-3 text-center d-flex flex-column abril-font">
           <h3>{props.name}</h3>
           <h6>{formattedPrice}</h6>
           <p>{props.description}</p>
-          <button className="btn btn-light" onClick={() => props.newView('details', idObject)}>Details</button>
+          <button className="btn btn-light btn-lg mt-auto" onClick={() => props.newView('details', idObject)}>Details</button>
         </div>
       </div>
     </div>
