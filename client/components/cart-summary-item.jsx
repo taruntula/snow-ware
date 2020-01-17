5e284137980de2026e3f549ce9bb500ec4155a7f
Tarun Padath <tarunpadath@gmail.com>
2020-01-16 19:45:19 -0800

    Added remove and edit button

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -3,18 +3,29 @@ import React from 'react';
 function CartSummaryItem(props) {
   const name = props.name;
   const image = props.image;
+  const quantity = props.count;
   const description = props.description;
-  const price = props.price;
+  const price = props.price * quantity;
   const formattedPrice = '$' + (price / 100).toFixed(2);
   return (
     <div className="row mb-2">
-      <div className="col-2 cart-height">
+      <div className="col-3 cart-height d-flex justify-content-center flex-column">
         <img className="img-fluid h-100" src={`../../images/${image}`} alt="" />
       </div>
-      <div className="col-10">
+      <div className="col-9">
         <h3>{name}</h3>
+        <h6>Qty. {quantity}</h6>
         <h6>{formattedPrice}</h6>
         <p>{description}</p>
+        <div className="row">
+          <div className="col-3">
+            <button className="btn btn-secondary">REMOVE</button>
+          </div>
+          <div className="col-9 pl-0">
+            <button className="btn btn-secondary">EDIT</button>
+          </div>
+
+        </div>
       </div>
     </div>
 
