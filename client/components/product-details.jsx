2cd2275c94e62de403eb7252588ca018ed5d7da2
Tarun Padath <tarunpadath@gmail.com>
2020-01-29 20:59:53 +0400

    Styled product details bigger font and better spacing

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -4,7 +4,8 @@ class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: null
+      product: null,
+      bigPic: ''
     };
   }
 
@@ -49,10 +50,26 @@ class ProductDetails extends React.Component {
               <img className="img-fluid details-height" src={`../../images/${this.state.product[0].images[0]}`} alt="" />
             </div>
             <div className="col-4 text-center">
-              <h3>{this.state.product[0].name}</h3>
-              <h6>{(this.state.product[0].price / 100).toFixed(2)}</h6>
-              <p>{this.state.product[0].shortDescription}</p>
-              <button className="btn btn-lg btn-secondary" onClick={() => this.props.addToCart(this.state.product[0])}>Add to Cart</button>
+              <div className="row">
+                <div className="col">
+                  <h1>{this.state.product[0].name}</h1>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col">
+                  <h3>{'$' + (this.state.product[0].price / 100).toFixed(2)}</h3>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col">
+                  <p>{this.state.product[0].shortDescription}</p>
+                </div>
+              </div>
+              <div className="row mt-3">
+                <div className="col">
+                  <button className="btn btn-lg btn-secondary" onClick={() => this.props.addToCart(this.state.product[0])}>Add to Cart</button>
+                </div>
+              </div>
             </div>
           </div>
           <div className="row mt-3">
