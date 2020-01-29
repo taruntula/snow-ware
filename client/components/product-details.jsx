6da4ba8999f78468ddbd6c76a5f43e6553be0070
Tarun Padath <tarunpadath@gmail.com>
2020-01-29 20:36:31 +0400

    Added hover to shift details page

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -6,7 +6,6 @@ class ProductDetails extends React.Component {
     this.state = {
       product: null
     };
-    this.handleOnDragStart = this.handleOnDragStart.bind(this);
   }
 
   componentDidMount() {
@@ -21,9 +20,6 @@ class ProductDetails extends React.Component {
       })
       .catch(error => console.error('Fetch failed', error));
   }
-  handleOnDragStart(event) {
-    event.preventDefault();
-  }
 
   render() {
     if (this.state.product === null) {
@@ -36,15 +32,27 @@ class ProductDetails extends React.Component {
               <h6> Back to catalog</h6>
             </button>
           </div>
-          <div className="mt-2 row h-50 align-items-center">
-            <div className="col-6">
-              <img className="img-fluid" src={`../../images/${this.state.product[0].images[0]}`} alt="" />
+          <div className="mt-2 row h-50">
+            <div className="col-2 d-flex flex-column">
+              <div className="row mb-3">
+                <div className="col text-center">
+                  <img className="img-fluid details-small" src={`../../images/${this.state.product[0].images[1]}`} alt="" />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col text-center">
+                  <img className="img-fluid details-small" src={`../../images/${this.state.product[0].images[2]}`} alt="" />
+                </div>
+              </div>
             </div>
             <div className="col-6 text-center">
+              <img className="img-fluid details-height" src={`../../images/${this.state.product[0].images[0]}`} alt="" />
+            </div>
+            <div className="col-4 text-center">
               <h3>{this.state.product[0].name}</h3>
               <h6>{(this.state.product[0].price / 100).toFixed(2)}</h6>
               <p>{this.state.product[0].shortDescription}</p>
-              <button className="btn btn-success" onClick={() => this.props.addToCart(this.state.product[0])}>Add to Cart</button>
+              <button className="btn btn-lg btn-secondary" onClick={() => this.props.addToCart(this.state.product[0])}>Add to Cart</button>
             </div>
           </div>
           <div className="row mt-3">
