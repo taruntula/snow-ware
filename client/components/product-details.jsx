f0bc056f2ab7bc6f94a38b03e584b4b3e14a8c28
Tarun Padath <tarunpadath@gmail.com>
2020-01-30 09:43:49 +0400

    Carousal not working

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -47,7 +47,35 @@ class ProductDetails extends React.Component {
               </div>
             </div>
             <div className="col-6 text-center">
-              <img className="img-fluid details-height" src={`../../images/${this.state.product[0].images[0]}`} alt="" />
+
+              <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img className="text-center details-height img-fluid" src={`../../images/${this.state.product[0].images[0]}`} alt="First slide" />
+                  </div>
+                  <div className="carousel-item">
+                    <img className="text-center details-height img-fluid" src={`../../images/${this.state.product[0].images[1]}`} alt="Second slide" />
+                  </div>
+                  <div className="carousel-item">
+                    <img className="text-center details-height img-fluid" src={`../../images/${this.state.product[0].images[2]}`} alt="Third slide" />
+                  </div>
+                </div>
+                <a className="carousel-control-prev btn-dark" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="sr-only">Previous</span>
+                </a>
+                <a className="carousel-control-next btn-dark" href="#carouselExampleIndicators" role="button" data-slide="next">
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="sr-only">Next</span>
+                </a>
+              </div>
+
+              {/* <img className="img-fluid details-height" src={`../../images/${this.state.product[0].images[0]}`} alt="" /> */}
             </div>
             <div className="col-4 text-center">
               <div className="row">
