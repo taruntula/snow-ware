f2fb8839cd744dd06a271c0884fe42f1f0c6f00e
Tarun Padath <tarunpadath@gmail.com>
2020-02-04 15:28:58 +0400

    quantity bring updated on database

---
+++ server/public/api/cart_patch.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once('functions.php');
+if (!INTERNAL) {
+  exit('no direct calls');
+}
+
+$input = getBodyData();
+$id = intval($input['id']);
+$addOrMinus = $input['addOrMinus'];
+
+if($addOrMinus != "add" && $addOrMinus != "minus") {
+  throw new Exception("Add or minus doesnt exist");
+}
+
+if ($id) {
+  if ($id <= 0) {
+    throw new Exception("Id is not greater than 0");
+  } else if (gettype($id) !== 'integer') {
+    throw new Exception("Id is not an integer");
+  }
+} else {
+  throw new Exception("Id not given");
+}
+
+if (!empty($_SESSION['cartId'])) {
+  $cartID = $_SESSION['cartId'];
+} else {
+  $cartID = false;
+}
+
+if ($addOrMinus === "add") {
+  $updateCartItemQuery = "UPDATE `cartItems` SET `count` = `count` + 1 WHERE `productID` = {$id} AND `cartID` = {$cartID}";
+  $updateCartItemResult = mysqli_query($conn, $updateCartItemQuery);
+  if (!$updateCartItemResult) {
+    throw new Exception("Sql error with increment cart items query" . mysqli_error($conn));
+  }
+}
+else {
+  $updateCartItemQuery = "UPDATE `cartItems` SET `count` = `count` - 1 WHERE `productID` = {$id} AND `cartID` = {$cartID}";
+  $updateCartItemResult = mysqli_query($conn, $updateCartItemQuery);
+  if (!$updateCartItemResult) {
+    throw new Exception("Sql error with decrement cart items query" . mysqli_error($conn));
+  }
+}
+
+// $deleteQuery = "DELETE FROM `cartItems` WHERE (`count` IS NULL OR `count` <= 0)";
+// $deleteQueryResult = mysqli_query($conn, $updateCartItemQuery);
+// if (!$deleteQueryResult) {
+//   throw new Exception("Sql error with delete cart items query" . mysqli_error($conn));
+// }
+
+?>
